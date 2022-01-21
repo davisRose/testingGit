c37c65e5d9ded70a50b7680d6ef7631b5bf7ab6e
Davis Rose <davisrose@Daviss-Big-Bitch.local>
2022-01-20 16:15:13 -0800

    trying to make red background show up

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
 function setup() {
   // put setup code here
   CreateCanvas(windowWidth, windowHeight);
-  background(230, 25, 25);
 }
 
 function draw() {
   // put drawing code here
+  background(230, 25, 25);
 }
